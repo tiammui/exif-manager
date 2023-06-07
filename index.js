1d9a0a00b7b3aa8b722abf7bb6566fb9b13efc8d
tiammui <48317927+tiammui@users.noreply.github.com>
2023-06-07 16:58:05 +0100

    UserComment

--- index.js
+++ index.js
@@ -8,22 +8,31 @@
 
 // Modules required for most of these exercises
 const { readFileSync } = require('fs');
-const { load, TAGS, ImageIFD, ExifIFD, GPSIFD } = require('piexifjs');
+const {
+  load,
+  dump,
+  insert,
+  TAGS,
+  ImageIFD,
+  ExifIFD,
+  GPSIFD,
+} = require('piexifjs');
 
 // Handy utility functions
 const getBinaryDataFromJpegFile = (filename) =>
   readFileSync(filename).toString('binary');
-const getBinaryDataFromBase64Data = (filename) => {
+const getBinaryDataFromBase64File = (filename) => {
   let data = readFileSync(filename);
   return Buffer.from(data.toString(), 'base64').toString('binary');
 };
 
 const getExifFromBase64File = (base64Filename) =>
-  load(getBinaryDataFromBase64Data(base64Filename));
+  load(getBinaryDataFromBase64File(base64Filename));
 const getExifFromJpegFile = (filename) =>
   load(getBinaryDataFromJpegFile(filename));
 
-console.log(getExifFromBase64File('./images/img.base64'));
+// console.log(getExifFromBase64File('./images/img.base64'));
+const getExifFromBinaryData = (binaryData) => load(binaryData);
 
 // Get the Exif data for the palm tree photos
 // (Assumes that the photos “palm tree 1.jpg” and “palm tree 2.jpg”
@@ -31,6 +40,44 @@ console.log(getExifFromBase64File('./images/img.base64'));
 const palm1Exif = getExifFromJpegFile('./images/palm tree 1.jpg');
 const palm2Exif = getExifFromJpegFile('./images/palm tree 2.jpg');
 const palmExifs = [palm1Exif, palm2Exif];
+
+/**
+ *
+ * @param binaryData binary format of the image to be updated (can use getBinaryDataFromBase64Data)
+ * @param mapInfo {string} JSON version of the map info
+ *
+ * @return the base64 of the updated image
+ */
+function updateUserComment(binaryData, mapInfo) {
+  // Load hotel photo
+  // (Assumes that the photo “hotel original.jpg”
+  // is in a directory named “images”)
+  const imageExif = getExifFromBinaryData(binaryData);
+
+  // Copy the original photo’s picture and Exif data, with UserComment
+  const newExif = {
+    '0th': { ...imageExif['0th'] },
+    Exif: { ...imageExif['Exif'] },
+    GPS: { ...imageExif['GPS'] },
+    Interop: { ...imageExif['Interop'] },
+    '1st': { ...imageExif['1st'] },
+    thumbnail: null,
+  };
+
+  newExif['Exif'][ExifIFD.UserComment] = mapInfo;
+
+  debugExif(newExif);
+
+  // Convert the new Exif object into binary form
+  const newExifBinary = dump(newExif);
+
+  // Embed the Exif data into the image data
+  const newPhotoData = insert(newExifBinary, binaryData);
+
+  // return the new photo as base64
+  return Buffer.from(newPhotoData, 'binary').toString('base64');
+}
+updateUserComment(getBinaryDataFromJpegFile('./images/palm tree 1.jpg'), 'man');
 
 // Show the Exif data for the two palm tree photos:
 // console.log(palm1Exif);
@@ -54,8 +101,8 @@ function debugExif(exif) {
     }
   }
 }
-debugExif(palm1Exif);
-debugExif(palm2Exif);
+// debugExif(palm1Exif);
+// debugExif(palm2Exif);
 
 // Show the Exif data for the two palm tree photos in
 // an easier to read format:
